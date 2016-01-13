b99669f6e8d6b3ff87b9a9562c0d263f725f081a
Antonina <zuch21@gmail.com>
2016-01-13 11:19:33 +0200

    commit

---
+++ forecast.php
@@ -0,0 +1,37 @@
+﻿<html>
+ <head>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link href="forecast_style.css" rel="stylesheet">
+ </head>
+ <body>
+  <div>
+    <p class="p_city">Ужгород</p>
+    <a href="index.php" class="a_other">інші населені пункти</a>
+    <div id="pic">
+
+      <img src="img/sun.png" width="100px">
+
+    </div>
+    <div id="text">
+      <p class="p_grad">12/01/2016</p>
+      <table>
+        <tr>
+          <td><img src="img/sun.png" width="30px" id="pic"></td>
+          <td>2-4</td>
+          <td><img src="img/sun.png" width="30px" id="pic"></td>
+          <td>2-4</td>
+        </tr>
+        <tr>
+          <td><img src="img/sun.png" width="30px" id="pic"></td>
+          <td>2-4</td>
+        </tr>
+        <tr>
+          <td><img src="img/sun.png" width="30px" id="pic"></td>
+          <td>2-4</td>
+        </tr>
+      </table>
+    </div>
+
+  </div>
+ </body>
+</html>
